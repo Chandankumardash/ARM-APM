33f5e396ede1e6ed89ed7a5339b94d41725eb719
Truptimayee-1 <truptimayee.mandal@bipros.com>
2023-02-24 19:49:54 +0530

    onsiteEmployees added

---
+++ force-app/main/default/lwc/onsiteEmployees/onsiteEmployees.js
@@ -0,0 +1,42 @@
+import { LightningElement } from "lwc";
+
+export default class Insurance extends LightningElement{
+
+
+    employees = [
+        {
+            "Id": "1231",   
+            "name": "Majority Sicely", 
+            "time":"Exceeded 2 days",
+            "active":"Active",
+            "desc":"Centrifugal valve repair",
+            "address":"US-Colorado-Denver-Plant-Area2-Floor1-East",
+            "reporting": "Reporting to:James Caviness",
+            "OnBoardDate": "01/08/2023",
+            "endDate": "1/22/2022",
+            
+        },
+        {
+            "Id": "1232",   
+            "name": "Mark Otto", 
+            "time":"",
+            "active":"Active",
+            "desc":"Installing and repairing Transformers",
+            "address":"US-Colorado-Denver-Plant-Area2-Floor1-East",
+            "reporting": "Reporting to:James Caviness",
+            "OnBoardDate": "01/13/2023",
+            "endDate": "1/24/2023",
+        },
+        {
+            "Id": "1233",   
+            "name": "John B.Roman", 
+            "time":"",
+            "active":"Active",
+            "desc":"Capital Overwhelming of Furnishing of crusher machine",
+            "address":"US-Colorado-Denver-Plant-Area2-Floor1-East",
+            "reporting": "Reporting to:James Caviness",
+            "OnBoardDate": "01/10/2023",
+            "endDate": "1/24/2022",
+        }
+    ]
+}
