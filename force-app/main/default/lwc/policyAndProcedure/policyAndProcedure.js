f634361de7a4efc469192bf10caa7c35466d9bad
chandan <chandan.dash@bipros.com>
2023-02-24 10:34:29 +0530

    test

---
+++ force-app/main/default/lwc/policyAndProcedure/policyAndProcedure.js
@@ -0,0 +1,32 @@
+import { LightningElement } from 'lwc';
+import paper_icon from "@salesforce/resourceUrl/paperIcon"
+
+export default class PolicyAndProcedure extends LightningElement {
+    paperIcon = paper_icon;
+    policies = [
+        {
+            "Id": "1231",   
+            "name": "Indemnification Agreement", 
+            "lastUpload": "06/22/2022",
+            "nextUpload": "12/22/2022",
+            "overdue": true,
+        },
+        {
+            "Id": "1232",   
+            "name": "Waste Disposal Agreement", 
+            "lastUpload": "10/13/2022",
+            "nextUpload": "10/04/2022",
+            "overdue": false,
+        },
+        {
+            "Id": "1233",   
+            "name": "Service Contract", 
+            "lastUpload": "08/02/2022",
+            "nextUpload": "08/02/2022",
+            "overdue": true,
+        },
+    ]
+    handelPolicyBack(){
+        this.dispatchEvent(new CustomEvent('click', {detail: "back"}));
+    }
+}
