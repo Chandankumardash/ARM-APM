579a88bfe6b5c68dc5d3fbc737460d440c0ca05a
Rahul Anand <rahul.anand435@gmail.com>
2023-02-24 11:17:36 +0530

    employee component added
    employee component added

---
+++ force-app/main/default/lwc/employee/employee.js
@@ -0,0 +1,51 @@
+import { LightningElement } from 'lwc';
+
+export default class Employee extends LightningElement {
+    employees = [
+        {
+            "Id": "EMP004",
+            "name": "Mark Otto",
+            "occupation": "Electrical",
+            "workStatus": "onsite",
+            "rating": 5,
+            "preference": "Top",
+            "Date": "Jun 28 2020"
+        },
+        {
+            "Id": "EMP004",
+            "name": "Mark Otto",
+            "occupation": "Electrical",
+            "workStatus": "onsite",
+            "rating": 5,
+            "preference": "Top",
+            "Date": "Jun 28 2020"
+        },
+        {
+            "Id": "EMP004",
+            "name": "Mark Otto",
+            "occupation": "Electrical",
+            "workStatus": "onsite",
+            "rating": 5,
+            "preference": "Top",
+            "Date": "Jun 28 2020"
+        },
+        {
+            "Id": "EMP004",
+            "name": "Mark Otto",
+            "occupation": "Electrical",
+            "workStatus": "onsite",
+            "rating": 5,
+            "preference": "Top",
+            "Date": "Jun 28 2020"
+        },
+        {
+            "Id": "EMP004",
+            "name": "Mark Otto",
+            "occupation": "Electrical",
+            "workStatus": "onsite",
+            "rating": 5,
+            "preference": "Top",
+            "Date": "Jun 28 2020"
+        }
+    ];
+}
