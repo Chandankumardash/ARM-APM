c6373d95f59e7a6b8a4114c29cd0a99adbdf7825
Nidhi Jha <nidhi.jha@bipros.com>
2023-02-28 20:35:48 +0530

    Observations
    Observations Added

---
+++ force-app/main/default/lwc/observations/observations.js
@@ -0,0 +1,17 @@
+import { LightningElement } from "lwc";
+
+export default class Insurance extends LightningElement{
+
+
+    observations = [
+        {
+            "Id": "1231",   
+            "name": "Performance and Safety Monitoring", 
+            "date": "Jan 13 2023 10:15 AM",
+            "wo":"Installing and repairing transformers",
+            "observed":"Richard S. Molina",
+            "type":"Safe Behavior",
+            "situation": "Low"
+        }
+    ]
+}
